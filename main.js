086014186f20aefe4b41c7f47d36c64523a3e15e
Michael Schonhardt <michael.schonhardt@gmail.com>
2023-04-23 20:20:53 +0200

    started page

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 new Vue({
     el: '#app',
     data: {
-      // Your data properties will be placed here
+      csvData: null,
+      tableColumns: ['Nummer', 'Datum', 'Ort', 'vonAn', 'Name_voll', 'GND', 'Wiki', 'Journalnummer', 'Aufbewahrungsort', 'Aufbewahrungsinstitution', 'Jahr', 'text', 'url', 'place_id', 'date', 'letter_text', 'status_letter_text']
     },
     methods: {
-        loadData() {
-          axios.get('data\register\final_register.csv')
-            .then(response => {
-              this.csvData = Papa.parse(response.data, { header: true }).data;
-            })
-            .catch(error => {
-              console.error('Error fetching CSV data:', error);
-            });
-          
-    },
+      loadData() {
+        axios.get('data/register/final_register.csv')
+          .then(response => {
+            this.csvData = Papa.parse(response.data, { header: true }).data;
+            console.log(this.csvData); 
+          })
+          .catch(error => {
+            console.error('Error fetching CSV data:', error);
+          });
+      }
     },
     mounted() {
       // Lifecycle hook to load data when the app is mounted
