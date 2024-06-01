36c053b6e7d9e5cc54e397731dd04ea06a8f61d6
Michael Schonhardt <michael.schonhardt@gmail.com>
2024-06-01 11:49:07 +0200

    added llama3 support

--- src/process_letter.py
+++ src/process_letter.py
@@ -22,6 +22,7 @@ class GetData():
         self.method = self.get_retrival_method_from_input()
         self.llm = self.get_llm_from_input()
         self.ids = self.get_ids_from_input()
+        self.processing_method = self.get_processing_method_from_input()
         self.list_of_persons = self.create_list_from_xml('person', 'lassberg-persons.xml')
         self.list_of_places = self.create_list_from_xml('place','lassberg-places.xml')
         self.list_of_literature = self.create_list_from_xml('bibl', 'lassberg-literature.xml')
@@ -44,6 +45,14 @@ class GetData():
     def get_retrival_method_from_input(self):
         user_input = input("Enter method of retrival: \"transkribus\", \"escriptorium\" or \"local\": ")
         if user_input in ["transkribus", "escriptorium", "local"]:
+            return user_input
+        else:
+                print(f"Invalid method: {user_input}")
+                exit()
+
+    def get_processing_method_from_input(self):
+        user_input = input("Enter method of processing: \"cpu\" or \"gpu\": ")
+        if user_input in ["cpu", "gpu"]:
             return user_input
         else:
                 print(f"Invalid method: {user_input}")
@@ -87,7 +96,9 @@ class GetData():
             if element == 'place':
                 list_of_items.append([item.get('{http://www.w3.org/XML/1998/namespace}id'), item.find('.//{*}placeName').get('ref'), re.sub('\s+',' ', item.find('.//{*}placeName').text.replace('\n',' '))])
             if element == 'bibl':
-                list_of_items.append([item.get('{http://www.w3.org/XML/1998/namespace}id'), item.find('.//{*}author').text])
+                list_of_items.append([item.get('{http://www.w3.org/XML/1998/namespace}id'), item.find('.//{*}idno').text, f"{item.find('.//{*}author').text} {item.find('.//{*}title').text}"])
+        print(list_of_items)
+        
         return list_of_items
 
 class ProcessText():
@@ -117,23 +128,25 @@ class NamedEntityRecognition():
         self.id = id
         self.letter_text = letter_text
         self.list_of_entities = self.flair_ner_extraction()
+
         # create gpt lookup strings with less tokens to reduce costs
         self.persons_gpt_lookup = self.create_gpt_lookup(self.data.list_of_persons)
         self.places_gpt_lookup = self.create_gpt_lookup(self.data.list_of_places)
-        #self.literature_gpt_lookup = self.create_gpt_lookup(self.data.list_of_literature)
+        self.literature_gpt_lookup = self.create_gpt_lookup(self.data.list_of_literature)
         self.ner_identification()
-        #self.persons = self.get_persons()
-        #self.places = self.get_places()
-        #self.misc = self.get_misc()
-
+        
     def flair_ner_extraction(self):
         print(f"\nStarting NER extraction for {self.id}")
         #log.log(f"\nStarting NER extraction for {self.id}")
         
         # load model
         tagger = Classifier.load('de-ner-large')
-        tagger.to('cpu')
-    
+        if self.data.processing_method == 'cpu':
+            tagger.to('cpu')
+        else:
+            tagger.to('cuda')
+
+
         sentence = Sentence(self.letter_text)
 
         tagger.predict(sentence)
@@ -159,49 +172,142 @@ class NamedEntityRecognition():
         
         return string_for_gpt
 
-    def identification_gpt4(self):
+    def identification_gpt4_persons(self):
         system_prompt = f'You are a machine build to retrieve information and will only return precisely the information you are asked for, without adding further smalltalk or information. You will also adhere to the format asked.'
         look_up_string_person = ''
-        look_up_string_places = ''
-        look_up_string_literature = ''
+        
         for item in self.list_of_entities:
             if item[1] == "PER":
                 look_up_string_person = look_up_string_person + item[0] + '|'
-            elif item[1] == "LOC":
-                look_up_string_places = look_up_string_places + item[0] + '|'
-            elif item[1] == "MISC":
-                look_up_string_literature = look_up_string_literature + item[0] + '|'
 
-        prompt_person_persons = f"""The following is a list of names of persons mentioned in a letter from the 19th century. 
+        prompt_person = f"""The following is a list of names of persons mentioned in a letter from the 19th century. 
                             It could refer to medieval figures as well as contemporary persons. Each person is separated by '|'. 
-                            Check for each person if it might be referenced in the following register and return the list I send you with the corresponding id in '()' after each name.
-                            Be aware that ther are propably no exact matches: There might be orthographic differences between the text given in the letter and the register, or abbreviations such as H. (Herr) or v. (von). Additionally, there might be functional words such as 'Graf' or 'Oberamtman', etc. in the text that are not part of the register.
-                            However, the person might not be in the register at all.
-                            If you cannot find a corrsponding match in the register, just return 'none' instead of the id. 
-                            It is important that the list keeps being seperated by '|'. This is the list of names to check: {look_up_string_person}. This is the register in the format 'id-1; name1|id-2; name2|': {self.persons_gpt_lookup}"""
+                            Check for each person if it might be referenced in the following register and return the list I 
+                            send you with the corresponding id in '()' after each name. Be aware that ther are propably no exact matches: 
+                            There might be orthographic differences between the text given in the letter and the register, 
+                            or abbreviations such as H. (Herr) or v. (von). Additionally, there might be functional words such as 'Graf' or 
+                            'Oberamtman', etc. in the text that are not part of the register. However, 
+                            the person might not be in the register at all. If you cannot find a corrsponding match in the register, 
+                            just return 'none' instead of the id. It is important that the list keeps being seperated by '|'. 
+                            This is the list of names to check: {look_up_string_person}. 
+                            This is the register in the format 'id-1; name1|id-2; name2|': {self.persons_gpt_lookup}"""
 
-        messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_person_persons}]
+        messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_person}]
         
         completion = client.chat.completions.create(model='gpt-4o',messages=messages)
         returned_list_persons_gpt4 = completion.choices[0].message
         print(returned_list_persons_gpt4)
+        return returned_list_persons_gpt4
+
+    def identification_gpt4_places(self):
+        system_prompt = f'You are a machine build to retrieve information and will only return precisely the information you are asked for, without adding further smalltalk or information. You will also adhere to the format asked.'
+        look_up_string_places = ''
+        for item in self.list_of_entities:
+            if item[1] == "LOC":
+                look_up_string_places = look_up_string_places + item[0] + '|'
+
+        prompt_places = f"""The following is a list of placenames mentioned in a letter from the 19th century. 
+                            Each place is separated by '|'. Check for each place if it might be referenced in 
+                            the following register and return the list I send you with the corresponding id in 
+                            '()' after each placename. 
+                            Be aware that ther are propably no exact matches: There might be orthographic 
+                            differences between the text given in the letter and the register, or abbreviations 
+                            such as 'Const.' for 'Konstanz' or 'Epp.' for 'Eppishausen. Additionally, 
+                            metaphors might beu sed such as 'Villa Epponis' for 'Eppishausen'. However, 
+                            the person might not be in the register at all.
+                            If you cannot find a corrsponding match in the register, 
+                            just return 'none' instead of the id. It is important that the list keeps being seperated by '|'. 
+                            This is the list of placenames to check: {look_up_string_places}. 
+                            This is the register in the format 'id-1; placename1|id-2; placename2|': {self.places_gpt_lookup}"""
+
+        messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_places}]
+        
+        completion = client.chat.completions.create(model='gpt-4o',messages=messages)
+        returned_list_places_gpt4 = completion.choices[0].message
+        print(returned_list_places_gpt4)
+        return returned_list_places_gpt4
+
+    def identification_gpt4_literature(self):
+        system_prompt = f'You are a machine build to retrieve information and will only return precisely the information you are asked for, without adding further smalltalk or information. You will also adhere to the format asked.'
+        look_up_string_literature = ''
+        for item in self.list_of_entities:
+            if item[1] == "MISC":
+                look_up_string_literature = look_up_string_literature + item[0] + '|'
+
+        if len(look_up_string_literature) > 0:
+
+            prompt_literature = f"""The following is a list of literature mentioned in a letter from the 19th century. 
+                                It could refer to abstract works as well as particular exemplars such as a medieval manuscript. 
+                                Each entity is separated by '|'. 
+                                Check for each entity if it might be referenced in the following register and return the list I 
+                                send you with the corresponding id in '()' after each entity. Be aware that ther are propably no exact matches: 
+                                There might be orthographic differences between the text given in the letter and the register, 
+                                or abbreviations and short titles such 'Tschudis Geschichte der Schweiz". However, 
+                                the entity might not be in the register at all. If you cannot find a corrsponding match in the register, 
+                                just return 'none' instead of the id. It is important that the list keeps being seperated by '|'. 
+                                This is the list of names to check: {look_up_string_literature}. 
+                                This is the register in the format 'id-1; name1|id-2; name2|': {self.literature_gpt_lookup}"""
+
+            messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_literature}]
+        
+            completion = client.chat.completions.create(model='gpt-4o',messages=messages)
+            returned_list_literature_gpt4 = completion.choices[0].message
+            print(returned_list_literature_gpt4)
+            return returned_list_literature_gpt4
+        
+        else:
+            return none
+
+    def identification_llama_persons(self):
+        look_up_string_person = ''
+        
+        for item in self.list_of_entities:
+            if item[1] == "PER":
+                look_up_string_person = look_up_string_person + item[0] + '|'
 
 
+        system_prompt = f'You are a machine build to retrieve information and will only return precisely the information you are asked for, without adding further smalltalk or information. You will also adhere to the format asked.'
+        prompt_person = f"""The following is a list of names of persons mentioned in a letter from the 19th century. 
+                            It could refer to medieval figures as well as contemporary persons. Each person is separated by '|'. 
+                            Check for each person if it might be referenced in the following register and return the list I 
+                            send you with the corresponding id in '()' after each name. Be aware that ther are propably no exact matches: 
+                            There might be orthographic differences between the text given in the letter and the register, 
+                            or abbreviations such as H. (Herr) or v. (von). Additionally, there might be functional words such as 'Graf' or 
+                            'Oberamtman', etc. in the text that are not part of the register. However, 
+                            the person might not be in the register at all. If you cannot find a corrsponding match in the register, 
+                            just return 'none' instead of the id. It is important that the list keeps being seperated by '|'. 
+                            This is the list of names to check: {look_up_string_person}. 
+                            This is the register in the format 'id-1; name1|id-2; name2|': {self.persons_gpt_lookup}"""
+        llm = Ollama(model="llama3", system=system_prompt)
+        print(llm.invoke(prompt_person))
 
+        return returned_list_persons
 
+    def identification_llama_places(self):
+        return returned_list_places
 
+    def identification_llama_literature(self):
+        return returned_list_literature
+        
 
+        
 
     def ner_identification(self):
         if self.data.llm == "llama3":
-            for entity in self.list_of_entities:
-                # TODO
-                llm = Ollama(model="llama3", system="You are a machine build for information retrival and will only return precisely the information you are asked for, a single id ending with four digits, without adding further smalltalk or information. If you don't know the answer, you will return 'none'.")
-                print(llm.invoke(f'"{entity[0]}" is a named entity extracted from a historical letter. Can you identify this entity in the following register and return its id? The register: {str(self.data.list_of_persons)}'))
+            returned_list_persons = self.identification_llama_persons()
+            returned_list_places = self.identification_llama_places()
+            returned_list_literature = self.identification_llama_literature()
+            
         elif self.data.llm == "gpt4":
-            self.identification_gpt4()
+            returned_list_persons = self.identification_gpt4_persons()
+            returned_list_places = self.identification_gpt4_places()
+            returned_list_literature = self.identification_gpt4_literature()
+            
         elif self.data.llm == "mock":
-            pass
+            returned_list_persons = "v. Ittner (0224)|Zapfs Monument (0419)|Trutpert Neugarts (0379)|Graven Mülinen (0075)|H. v. Arx (0169)|Anshelms (0467)|Justinger (0377)|Tschachtlan (0378)|von Constanz (0058)|Joseph v. Laßberg (0373)|"
+            returned_list_places = "Eppishausen (0043)|Augsburg (0012)|Bern (0020)|Rozmital (none)|"
+            returned_list_literature = "Raumers Geschichte der Hohenstaufen (lassberg-literature-0010)|Episcopatus Constantiensis (lassberg-literature-0012)|Appenzeller Krieges (none)"
+            
 
 
 def main():
